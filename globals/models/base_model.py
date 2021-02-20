58cc7f84450ed8c02f4cc4d83becbb5916939918
Thalles Domician <thalles.oliveira@pravaler.com.br>
2021-02-19 23:35:42 -0300

    reorganizei globasls com model base

--- globals/models/base_model.py
+++ globals/models/base_model.py
@@ -7,6 +7,11 @@ class BaseModel(models.Model):
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+
+    #TODO: Criar manager para buscar apenas Models com created_at null or blank
+    # https://docs.djangoproject.com/en/3.1/topics/db/managers/ 
 
     def delete(self, using=None, keep_parents=False):
         self.active = False
